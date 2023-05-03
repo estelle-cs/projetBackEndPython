8ba3bb7dd483ce26e86bd923bac4bfe3f0afc100
estelle-cs <casu.estelle@yahoo.com>
2023-05-03 12:34:28 +0200

    Improvement : update models

--- app/internal/models.py
+++ app/internal/models.py
@@ -1,10 +1,31 @@
 #System imports
 
 #Libs imports
+import datetime
+import typing
 from pydantic import BaseModel
 
 #Local imports
 
+
 class User(BaseModel): # we don't include password_hash in the definition of the class because we don't want to return it
     id: int
     name: str
+    surname: str
+    email: str
+    idCompany: int
+    admin: bool
+
+class Company(BaseModel):
+    id: int
+    name: str
+
+class Planning(BaseModel):
+    id: int
+
+class Activity(BaseModel):
+    id: int
+    day: datetime.date
+    startTime: datetime.time
+    endTime: datetime.time
+    concernedUsers: typing.List
