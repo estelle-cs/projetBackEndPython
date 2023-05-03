c0591d3540af79b356960f37a4bd013a53524fb6
estelle-cs <casu.estelle@yahoo.com>
2023-05-03 12:34:12 +0200

    Remove : Old route

--- app/main.py
+++ app/main.py
@@ -4,11 +4,10 @@
 from fastapi import FastAPI, Response, status
 
 #Local imports
-from routers import cars, users
+from routers import users
 from internal import auth
 
 app = FastAPI()
 
 app.include_router(auth.router, tags=["Authentification"])
-app.include_router(cars.router, tags=["Car"])
 app.include_router(users.router, tags=["User"])
